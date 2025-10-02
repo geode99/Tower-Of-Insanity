70f259f0c761d4ab1cb1b890f9b717d1ca4fee72
MUKILTEO\1725219 <george.chmykhalo@gmail.com>
2025-10-02 09:41:28 -0700

    Sanity beginning and Wall Tilemap

--- Schizo Wizard Tower Unity/Assets/Scripts/PlayerSanity.cs
+++ Schizo Wizard Tower Unity/Assets/Scripts/PlayerSanity.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerSanity : MonoBehaviour
 {
+    public float sanity = 100f;
+    public float sanityRange = 1f;
+    public float range = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,7 @@ public class PlayerSanity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        sanity -= 0.05f * Time.deltaTime;
+        sanityRange = sanity*range;
     }
 }
