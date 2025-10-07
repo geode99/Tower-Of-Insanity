04b1b1d8ff7d0205ccf5a3825030b23b3fbb25bd
MUKILTEO\1726098 <uberfrogguy@gmail.com>
2025-10-07 10:08:47 -0700

    working on UI AGAIN

--- Schizo Wizard Tower Unity/Assets/Scripts/Inventory Stuff/PlayerItemCollector.cs
+++ Schizo Wizard Tower Unity/Assets/Scripts/Inventory Stuff/PlayerItemCollector.cs
@@ -6,7 +6,7 @@ public class PlayerItemCollector : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        inventoryController = FindObjectOfType<InventoryController>();
+        inventoryController = FindFirstObjectByType<InventoryController>();
     }
 
    private void OnTriggerEnter2D(Collider2D collision)
