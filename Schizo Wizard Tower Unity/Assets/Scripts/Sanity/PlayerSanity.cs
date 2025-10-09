ac01d35f68b3c56dc6b236b8d82cdb0489ca2702
MUKILTEO\1725219 <george.chmykhalo@gmail.com>
2025-10-09 09:39:31 -0700

    Master Schizo Wizard

--- Schizo Wizard Tower Unity/Assets/Scripts/Sanity/PlayerSanity.cs
+++ Schizo Wizard Tower Unity/Assets/Scripts/Sanity/PlayerSanity.cs
@@ -21,7 +21,7 @@ public class PlayerSanity : MonoBehaviour
         if(sanity > 0){
             sanity -= 0.1f * Time.deltaTime;
         }
-        sanityBar.transform.localScale = new Vector3(4f, sanity/10, 1f);
+        sanityBar.transform.localScale = new Vector3(1f, sanity/40, 1f);
         sanityRange = sanity * range;
         sphere.SetSphereScale(sanityRange);
     }
