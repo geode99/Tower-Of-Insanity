02a4e146edddf4ac73cdfcbcdaa28f6f6a740f1e
MUKILTEO\1725219 <george.chmykhalo@gmail.com>
2025-10-09 08:58:24 -0700

    Merge issues

--- Schizo Wizard Tower Unity/Assets/Scripts/PlayerMovement.cs
+++ Schizo Wizard Tower Unity/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,16 +8,24 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movement;
 
     private Rigidbody2D _rb;
+    private Animator _animator;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
-
         _rb.linearVelocity = _movement * _moveSpeed;
+    }
+
+    public void move(InputAction.CallbackContext ctx)
+    { 
+        _movement = ctx.ReadValue<Vector2>();
+
+        _animator.SetFloat("x", _movement.x);
+        _animator.SetFloat("y", _movement.y);
     }
 }
