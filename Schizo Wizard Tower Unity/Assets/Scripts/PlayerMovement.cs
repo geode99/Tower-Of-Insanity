f98a6f8aca96b8f9cc9138feb781a29bc8c9024e
MUKILTEO\1725219 <george.chmykhalo@gmail.com>
2025-10-30 09:03:59 -0700

    Movement COMPLETE

--- Schizo Wizard Tower Unity/Assets/Scripts/PlayerMovement.cs
+++ Schizo Wizard Tower Unity/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask whatStopsMovement;
     public float previousX;
     public float previousY;
-    public bool canMove = true;
+    public Animator animator;
     void Start(){
         movePoint.parent = null;
     }
-    private void Update() {
+    private void FixedUpdate() {
 
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
 
@@ -21,14 +21,22 @@ public class PlayerMovement : MonoBehaviour
                 if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .25f, whatStopsMovement)){
                     previousX = movePoint.position.x;
                     previousY = movePoint.position.y;
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") * 0.5f, 0f, 0f);
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
-            }else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f){
+                animator.SetFloat("x", Input.GetAxisRaw("Horizontal"));
+                animator.SetFloat("y", 0);
+            }
+            else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f){
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement)){
                     previousX = movePoint.position.x;
                     previousY = movePoint.position.y;
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical") * 0.5f, 0f);
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                 }
+                animator.SetFloat("x", 0);
+                animator.SetFloat("y", Input.GetAxisRaw("Vertical"));
+            }else{
+                animator.SetFloat("x", 0);
+                animator.SetFloat("y", 0);
             }
         }
     }
